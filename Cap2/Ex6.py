fdd28a8e0fa44dea0e248e7d63d55d5e99b267d1
Fernando Puebla Stein <101823993+Stenishh@users.noreply.github.com>
2025-02-06 15:36:56 -0300

    Update Ex6

--- Cap2/Ex6.py
+++ Cap2/Ex6.py
@@ -1,15 +1,12 @@
 import math
 
-# Solicita um número decimal ao usuário
 numero = float(input("Digite um número decimal: "))
 
-# Calcula os valores solicitados
 raiz_quadrada = math.sqrt(numero)
 teto = math.ceil(numero)
 chao = math.floor(numero)
 parte_inteira = int(numero)
 
-# Exibe os resultados
 print("Raiz quadrada:", raiz_quadrada)
 print("Função teto:", teto)
 print("Função chão:", chao)
